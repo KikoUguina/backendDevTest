f77d181a5d794e35c3cd2ca77387aa99bd64d7a5
KikoUguina <kipuguina@gmail.com>
2025-04-23 22:06:26 +0200

    feat: añade endpoint GET /tasks con lista en memoria

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "API ACTIVA"}
+class Task(BaseModel):
+    title: str
+    description: str
+
+tasks: List[Task] = []
+
+@app.get("/tasks", response_model=List[Task])
+def get_tasks():
+    return tasks
